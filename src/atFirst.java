e82e5d8e9b3070266100f99c3cd73af9810a2755
Vision666 <bvision666@gmail.com>
2018-05-05 09:28:18 +0300

    Another text addition

--- src/atFirst.java
+++ src/atFirst.java
@@ -2,5 +2,6 @@ public class atFirst {
     public static void main(String args[]){
         String str = "";
         System.out.println(2-str.length());
+        System.out.println("Another text form IDEA");
     }
 }
