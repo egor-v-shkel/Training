886124528f364b4d49b83d20f7d9812b749316ef
Vision666 <bvision666@gmail.com>
2018-07-19 16:07:40 +0300

    Class

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,17 @@
-//import Array1.*;
-//import Array3.*;
-//import String2.*;
+import CodeWars.Mixing;
 
-import CodeWars.NextBiggerNumber;
-
-import java.util.Arrays;
+import static CodeWars.Mixing.mix;
 
 public class Main {
     public static void main(String args[]) {
-        System.out.println(NextBiggerNumber.nextBiggerNumber(2017));
+        System.out.println(Mixing.mix("Are they here", "yes, they are here"));
+        //String testString = "Are they here aaa";
+
+        /*int split = testString.split("[e]",-1).length-1;
+        System.out.println("split = " + split);*/
+
+        //long java8Case2 = testString.codePoints().filter(ch -> ch =='a').count();
+        //System.out.println("java8 (second case) = " + java8Case2);
     }
 }
 
