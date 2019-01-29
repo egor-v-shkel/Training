005798638262bd537cb2ce744e0d6f914cada0cb
Vision666 <bvision666@gmail.com>
2019-01-29 21:22:14 +0300

    CodeWars

---
+++ src/Schildt/LambdaExpressions/LambdaDemo3.java
@@ -0,0 +1,17 @@
+package Schildt.LambdaExpressions;
+
+@FunctionalInterface
+interface StringFunc{
+
+    String func(String str);
+
+}
+
+public class LambdaDemo3 {
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
